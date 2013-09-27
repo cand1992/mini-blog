aad564cc18b4f9f37629ef3d788a2b58bd644c20
DevPoint 5 <devpoint5@DevPoints-iMac-4.local>
2013-09-27 16:33:11 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniBlog::Application.config.secret_token = 'f525d606feedfc2f4660699fc6e74b6e46adfd28581ab2c370ee1d308ef268c81471674def55dddde12ae9c058da1198ee0cb3d20cc6c5b20c1b201d18919de7'
