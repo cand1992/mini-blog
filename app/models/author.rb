ce6b1252b6bdbc4479ab2a4f40c4e02c81ac224f
cand1992 <christopher_anderson@mac.com>
2013-10-09 17:05:48 -0600

    adding simple form, refactoring author sign up form to use it

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,11 +4,12 @@ class Author < ActiveRecord::Base
   has_many :comments, through: :posts
   attr_accessible :bio, :email, :first_name, :last_name, :twitter, :username, :password, :password_confirmation
   #presence
+  validates :password, :password_confirmation, presence: true, on: :create
   validates :email, :first_name, :last_name, :username, presence: true
   #uniqueness
   validates :email, :username, :twitter, uniqueness: true, allow_blank: true
   #length
-  validates :bio, length: {in: 30..500}
+  validates :password, :password_confirmation, length: {in: 6..24}
   #validates :email, presence: true,
    #                 uniqueness: true,
    #                 length: { minimum: 3}
